edffb2acf776eb7b782d8bf39f07bc66369c366c
sofien6 <foufou.hedjazi@gmail.com>
2022-09-30 00:30:11 +0100

    add insert query

--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,19 @@ var connnection = require('../db_config')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  var getStudents = " select * from student"
+  var getStudents = " select * from student where (age = 21)  "
   connnection.query(getStudents,(err,result)=>{
      if(err) console.log(' there is an error ');
      else res.send({result})
   })
 });
+
+router.post('/add-user',(req,res)=>{
+     var addUser = " INSERT INTO student (firstName,lastName,age) VALUES ('sofien', 'hedjazi',35) "
+     connnection.query(addUser,(err,result)=>{
+      if(err) console.log(' there is an error ');
+      else res.send({result})
+     })
+})
 
 module.exports = router;
